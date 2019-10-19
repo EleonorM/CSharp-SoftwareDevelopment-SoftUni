3919c173622386f4c3d17e0b79afa667119093fa
Eleonor Manolova <eleonor.manolova@gmail.com>
2019-10-19 14:46:13 +0300

    Added solutions

---
+++ C# TechModule/01.2Intro and Basic Syntax - Exercise/05. Login/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] user = input.ToCharArray();
+            char[] password = new char[user.Length];
+            for (int i = 0; i < user.Length; i++)
+            {
+                password[i] = user[user.Length - i-1];
+            }
+            int counter = 0;
+            while (true)
+            {
+                char[] word = Console.ReadLine().ToCharArray();
+                if (Enumerable.SequenceEqual(word, password))
+                {
+                    Console.WriteLine($"User {string.Join("",user)} logged in.");
+                    return;
+                }
+                else
+                {
+                    counter++;
+                    if (counter == 4)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                
+            }
+            Console.WriteLine($"User {string.Join("",user)} blocked!");
+
+        }
+    }
+}
