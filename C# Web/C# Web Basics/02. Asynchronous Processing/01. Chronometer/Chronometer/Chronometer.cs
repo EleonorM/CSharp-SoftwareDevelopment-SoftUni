8eba106eb79d9b677f0abbd7e30e1b4960e81052
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-01-20 19:09:57 +0200

    Add solutions to 02. Asynchronous Processing

---
+++ C# Web/C# Web Basics/02. Asynchronous Processing/01. Chronometer/Chronometer/Chronometer.cs
@@ -0,0 +1,45 @@
+﻿namespace Chronometer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    public class Chronometer : IChronometer
+    {
+        private Stopwatch stopwatch;
+
+        public string GetTime => stopwatch.Elapsed.ToString();
+
+        public List<string> Laps { get; private set; }
+
+        public Chronometer()
+        {
+            stopwatch = new Stopwatch();
+            Laps = new List<string>();
+        }
+
+        public string Lap()
+        {
+           var timespan = stopwatch.Elapsed;
+            Laps.Add(timespan.ToString());
+            return timespan.ToString();
+        }
+
+        public void Reset()
+        {
+            stopwatch.Restart();
+            stopwatch.Stop();
+            Laps = new List<string>();
+        }
+
+        public void Start()
+        {
+            stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            stopwatch.Stop();
+        }
+    }
+}
---
+++ C# Web/C# Web Basics/SIS.HTTP/SIS.WebServer/ConnectionHandler.cs
@@ -0,0 +1,104 @@
+﻿namespace SIS.WebServer
+{
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Exceptions;
+    using SIS.HTTP.Requests;
+    using SIS.HTTP.Responses;
+    using SIS.WebServer.Results;
+    using SIS.WebServer.Routing;
+    using System;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ConnectionHandler
+    {
+        private readonly Socket client;
+
+        private readonly IServerRoutingTable serverRoutingTable;
+
+        public ConnectionHandler(Socket client, IServerRoutingTable serverRoutingTable)
+        {
+            CoreValidator.ThrowIfNull(client, nameof(client));
+            CoreValidator.ThrowIfNull(serverRoutingTable, nameof(serverRoutingTable));
+
+            this.client = client;
+            this.serverRoutingTable = serverRoutingTable;
+        }
+
+        public async Task ProcessRequestAsync()
+        {
+            try
+            {
+                var httpRequest = await this.ReadRequest();
+
+                if (httpRequest != null)
+                {
+                    Console.WriteLine($"Processing: {httpRequest.RequestMethod} {httpRequest.Path}...");
+
+                    var httpResponse = HandleRequest(httpRequest);
+
+                    await this.PrepareResponse(httpResponse);
+                }
+            }
+            catch (BadRequestException be)
+            {
+                await this.PrepareResponse(new TextResult(be.ToString(), HttpResponseStatusCode.BadRequest));
+            }
+            catch (Exception e)
+            {
+                await PrepareResponse(new TextResult(e.ToString(), HttpResponseStatusCode.InternalServerError));
+            }
+
+            this.client.Shutdown(SocketShutdown.Both);
+        }
+
+        private async Task<IHttpRequest> ReadRequest()
+        {
+            var result = new StringBuilder();
+            var data = new ArraySegment<byte>(new byte[1024]);
+
+            while (true)
+            {
+                int numberOfBytesRead = await this.client.ReceiveAsync(data.Array, SocketFlags.None);
+
+                if (numberOfBytesRead == 0)
+                {
+                    break;
+                }
+
+                var bytesAsString = Encoding.UTF8.GetString(data.Array, 0, numberOfBytesRead);
+                result.Append(bytesAsString);
+
+                if (numberOfBytesRead < 1023)
+                {
+                    break;
+                }
+            }
+
+            if (result.Length == 0)
+            {
+                return null;
+            }
+
+            return new HttpRequest(result.ToString());
+        }
+
+        private IHttpResponse HandleRequest(IHttpRequest httpRequest)
+        {
+            if (!serverRoutingTable.Contains(httpRequest.RequestMethod, httpRequest.Path))
+            {
+                return new TextResult($"Route with method {httpRequest.RequestMethod} and path \"{httpRequest.Path}\" not found.", HttpResponseStatusCode.NotFound);
+            }
+
+            return serverRoutingTable.Get(httpRequest.RequestMethod, httpRequest.Path).Invoke(httpRequest);
+        }
+
+        private async Task PrepareResponse(IHttpResponse httpResponse)
+        {
+            byte[] byteSegments = httpResponse.GetBytes();
+            await client.SendAsync(byteSegments, SocketFlags.None);
+        }
+    }
+}
