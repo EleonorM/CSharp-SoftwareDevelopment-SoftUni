b4c6f7c8a0fa5a50db8929b40061caef2bcd930b
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-06-14 12:25:50 +0300

    Add solution to DataStuctures-Trees

---
+++ Data Structures/(02.2016)3.1 Trees and Tree-Like Structures-Lab/Binary-Tree/BinaryTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public T Value { get; set; }
+
+    public BinaryTree<T> LeftChild { get; set; }
+
+    public BinaryTree<T> RightChild { get; set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        Value = value;
+        LeftChild = leftChild;
+        RightChild = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', 2 * indent) + Value);
+        if (LeftChild != null)
+        {
+            LeftChild.PrintIndentedPreOrder(indent + 1);
+        }
+
+        if (RightChild != null)
+        {
+            RightChild.PrintIndentedPreOrder(indent + 1);
+        }
+
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (LeftChild != null)
+        {
+            LeftChild.EachInOrder(action);
+        }
+
+        action(Value);
+        if (RightChild != null)
+        {
+            RightChild.EachInOrder(action);
+        }
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (LeftChild != null)
+        {
+            LeftChild.EachPostOrder(action);
+        }
+
+        if (RightChild != null)
+        {
+            RightChild.EachPostOrder(action);
+        }
+
+        action(Value);
+    }
+}
---
+++ Data Structures/(02.2016)3.1 Trees and Tree-Like Structures-Lab/Binary-Tree/Tree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public T Value { get; }
+
+    public IList<Tree<T>> Children { get; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        Value = value;
+        Children = new List<Tree<T>>();
+        foreach (var child in children)
+        {
+            Children.Add(child);
+        }
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', 2 * indent) + Value);
+        foreach (var child in Children)
+        {
+            child.Print(indent + 1);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(Value);
+        foreach (var child in Children)
+        {
+            child.Each(action);
+        }
+    }
+}
