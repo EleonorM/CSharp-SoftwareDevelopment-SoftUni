254c8e4d1288abafa5636d2f643443b17d891f12
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-06-28 17:10:26 +0300

    Add soluion to Data Stuctures BFS DFS

---
+++ Data Structures/(02.2016)4.1 BFS DFS/BFS-Escape-from-Labyrinth/EscapeFromLabyrinth.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class EscapeFromLabyrinth
+{
+    static int width;
+
+    static int heigth;
+
+    static char[,] labirinth;
+    public static void Main()
+    {
+        ReadLabirinth();
+      var path =  FindShortestPathToExit();
+        PrintPath(path);
+    }
+
+    private static void PrintPath(string path)
+    {
+        if (path == null)
+        {
+            Console.WriteLine("No exit!");
+        }
+        else if (path =="")
+        {
+            Console.WriteLine("Start is at the exit.");
+        }
+        else
+        {
+            Console.WriteLine("Shortest exit: " + path);
+        }
+    }
+
+    private static void ReadLabirinth()
+    {
+        width = int.Parse(Console.ReadLine());
+        heigth = int.Parse(Console.ReadLine());
+        labirinth = new char[width, heigth];
+
+        for (int row = 0; row < heigth; row++)
+        {
+            var input = Console.ReadLine();
+            var rowInput = input.ToCharArray();
+            for (int column = 0; column < rowInput.Length; column++)
+            {
+
+                labirinth[column, row] = rowInput[column];
+            }
+        }
+    }
+
+    class Point
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public string Direction { get; set; }
+
+        public Point PrevPoint { get; set; }
+    }
+
+    static char StartPointCell = 's';
+
+    static char VisitedCell = 'v';
+
+    private static string FindShortestPathToExit()
+    {
+        var queue = new Queue<Point>();
+        var startPoint = FindStartPoint();
+        if (startPoint == null)
+        {
+            return null;
+        }
+
+        queue.Enqueue(startPoint);
+        while (queue.Count != 0)
+        {
+            var currentCell = queue.Dequeue();
+            if (ItIsOnBorder(currentCell))
+            {
+                return TraceBackPath(currentCell);
+            }
+
+            TryDirection(queue, currentCell, "U", 0, -1);
+            TryDirection(queue, currentCell, "D", 0, 1);
+            TryDirection(queue, currentCell, "L", -1, 0);
+            TryDirection(queue, currentCell, "R", 1, 0);
+        }
+
+        return null;
+    }
+
+    private static void TryDirection(Queue<Point> queue, Point currentCell, string direction, int x, int y)
+    {
+        int newX = currentCell.X + x;
+        int newY = currentCell.Y + y;
+
+        if (newX >= 0 && newX < width && newY >= 0 && newY < heigth && labirinth[newX, newY] == '-')
+        {
+            labirinth[newX, newY] = VisitedCell;
+
+            var nextCell = new Point
+            {
+                X = newX,
+                Y = newY,
+                PrevPoint = currentCell,
+                Direction = direction,
+            };
+            queue.Enqueue(nextCell);
+        }
+    }
+
+    private static string TraceBackPath(Point currentCell)
+    {
+        var path = new List<string>();
+        while (currentCell.PrevPoint != null)
+        {
+            path.Add(currentCell.Direction);
+            currentCell = currentCell.PrevPoint;
+        }
+
+        path.Reverse();
+        var pathAsString = new StringBuilder();
+        foreach (var diretion in path)
+        {
+            pathAsString.Append(diretion);
+        }
+
+        return pathAsString.ToString().TrimEnd();
+    }
+
+    private static bool ItIsOnBorder(Point currentCell)
+    {
+        var isOnBorderX = currentCell.X == 0 || currentCell.X == width - 1;
+        var isOnBorderY = currentCell.Y == 0 || currentCell.Y == heigth - 1;
+        return isOnBorderX || isOnBorderY;
+    }
+
+    private static Point FindStartPoint()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < heigth; j++)
+            {
+                if (labirinth[i, j] == StartPointCell)
+                {
+                    return new Point { X = i, Y = j };
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ Data Structures/(02.2016)4.1 BFS DFS/DFS-Graph-Traversal/GraphConnectedComponents.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static new List<int>[] graph = new List<int>[]
+             {
+            new List<int>(){3,6},
+            new List<int>(){3,4,5,6},
+            new List<int>(){8},
+            new List<int>(){0,1,5},
+            new List<int>(){1,6},
+            new List<int>(){1,3},
+            new List<int>(){0,1,4},
+            new List<int>(){},
+            new List<int>(){2}
+             };
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+    private static List<int>[] ReadGraph()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            var nums = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            graph[i] = nums;
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        isVisited = new bool[graph.Length];
+        var lastCheckedNode = 0;
+        if (graph.Length == 0)
+        {
+            Console.Write("");
+            return;
+        }
+        do
+        {
+            var sequence = new List<int>();
+            DFS(lastCheckedNode, ref sequence);
+            if (sequence.Count != 0)
+            {
+                PrintNodes(sequence);
+            }
+
+            lastCheckedNode++;
+        } while (lastCheckedNode != graph.Length);
+
+    }
+
+    static bool[] isVisited;
+
+    static void DFS(int node, ref List<int> sequence)
+    {
+        if (!isVisited[node])
+        {
+            isVisited[node] = true;
+            foreach (var childNode in graph[node])
+            {
+                DFS(childNode, ref sequence);
+            }
+            sequence.Add(node);
+
+        }
+    }
+
+    private static void PrintNodes(List<int> nodes)
+    {
+        Console.WriteLine("Connected component: " + string.Join(" ", nodes));
+    }
+}
