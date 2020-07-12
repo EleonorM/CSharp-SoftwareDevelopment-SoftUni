86b9ea3e986001927e131db33e10f1e758eeed29
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-07-12 11:27:16 +0300

    Add solution to Data Structures

---
+++ Data Structures/(02.2016)6.1 Collection Data Structures and Libraries Lab/01. EventsInGivenDateRange/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _01._EventsInGivenDateRange
+{
+    using System;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var linesToRead = int.Parse(Console.ReadLine());
+
+
+            var collectionOfCoursesAndDates = new OrderedMultiDictionary<DateTime, string>(true);
+            for (int i = 0; i < linesToRead; i++)
+            {
+                var courseAndDate = Console.ReadLine().Split(" | ");
+                var course = courseAndDate[0];
+                var date = DateTime.Parse(courseAndDate[1]);
+                collectionOfCoursesAndDates.Add(date, course);
+            }
+
+            var linesToReadRange = int.Parse(Console.ReadLine());
+            for (int i = 0; i < linesToReadRange; i++)
+            {
+                var fromDateAndToDate = Console.ReadLine().Split(" | ");
+                var fromDate = DateTime.Parse(fromDateAndToDate[0]);
+                var toDate = DateTime.Parse(fromDateAndToDate[1]);
+                var eventsInRange = collectionOfCoursesAndDates.Range(fromDate, true, toDate, true);
+                Console.WriteLine(eventsInRange.Values.Count);
+                foreach (var eventInRange in eventsInRange)
+                {
+                    if (eventInRange.Value.Count > 1)
+                    {
+                        foreach (var eventValue in eventInRange.Value)
+                        {
+                            Console.WriteLine($"{eventValue} | {eventInRange.Key:dd-MMM-yyyy}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{eventInRange.Value.First()} | {eventInRange.Key:dd-MMM-yyyy}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Data Structures/(02.2016)6.1 Collection Data Structures and Libraries Lab/02. RopeForEfficientStringEditing/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02._RopeForEfficientStringEditing
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var rope = new BigList<char>();
+
+            var input = Console.ReadLine();
+            while (input != "PRINT")
+            {
+                var inputParts = input.Split(" ");
+                if (inputParts[0] == "INSERT")
+                {
+                    rope.InsertRange(0, inputParts[1].ToCharArray());
+                }
+                else if (inputParts[0] == "APPEND")
+                {
+                    rope.InsertRange(rope.Count, inputParts[1].ToCharArray());
+                }
+                else if (inputParts[0]== "DELETE")
+                {
+                    try
+                    {
+                        rope.RemoveRange(int.Parse(inputParts[1]), int.Parse(inputParts[2]));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("ERROR");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+                Console.WriteLine("OK");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join("",rope));
+        }
+    }
+}
