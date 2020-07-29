dd6487a8c23e2e04653f571608344bb3090c84af
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-07-29 16:22:42 +0300

    Add solution to Alogrithms Dynamic Programming Lab

---
+++ Algorithms/(04.2016)4.1 Dynamic Programming Lab/Longest-Common-Subsequence/LongestCommonSubsequence.cs
@@ -0,0 +1,80 @@
+﻿namespace Longest_Common_Subsequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class LongestCommonSubsequence
+    {
+        public static void Main()
+        {
+            // Judge Solution
+            // var firstStr = Console.ReadLine();
+            // var secondStr = Console.ReadLine();
+            // var lcs = FindLongestCommonSubsequence(firstStr, secondStr);
+            // Console.WriteLine(lcs.Length);
+
+            var firstStr = "tree";
+            var secondStr = "team";
+
+            var lcs = FindLongestCommonSubsequence(firstStr, secondStr);
+
+            Console.WriteLine("Longest common subsequence:");
+            Console.WriteLine("  first  = {0}", firstStr);
+            Console.WriteLine("  second = {0}", secondStr);
+            Console.WriteLine("  lcs    = {0}", lcs);
+        }
+
+        public static string FindLongestCommonSubsequence(string firstStr, string secondStr)
+        {
+            int lcsLenght = firstStr.Length + 1;
+            int lcsWidth = secondStr.Length + 1;
+            var lcs = new int[lcsLenght, lcsWidth];
+            lcs[0, 0] = 0;
+
+            for (int i = 1; i < lcsLenght; i++)
+            {
+                for (int j = 1; j < lcsWidth; j++)
+                {
+                    if (firstStr[i - 1] == secondStr[j - 1])
+                    {
+                        lcs[i, j] = lcs[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        lcs[i, j] = Math.Max(lcs[i, j - 1], lcs[i - 1, j]);
+                    }
+                }
+            }
+
+            return RetrieveLCS(firstStr, secondStr, lcs);
+        }
+
+        private static string RetrieveLCS(string firstStr, string secondStr, int[,] lcs)
+        {
+            var sequence = new List<char>();
+            int i = firstStr.Length;
+            int j = secondStr.Length;
+
+            while (i > 0 && j > 0)
+            {
+                if (firstStr[i - 1] == secondStr[j - 1])
+                {
+                    sequence.Add(firstStr[i - 1]);
+                    i--;
+                    j--;
+                }
+                else if (lcs[i, j] == lcs[i - 1, j])
+                {
+                    i--;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+
+            sequence.Reverse();
+            return new string(sequence.ToArray());
+        }
+    }
+}
---
+++ Algorithms/(04.2016)4.1 Dynamic Programming Lab/Longest-Increasing-Subsequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,64 @@
+﻿namespace Longest_Increasing_Subsequence
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LongestIncreasingSubsequence
+    {
+        public static void Main()
+        {
+            // Judge Solution
+            // var sequence = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            // var longestSeq = FindLongestIncreasingSubsequence(sequence);
+            // Console.WriteLine(string.Join(" ", longestSeq));
+
+
+            var sequence = new[] { 3, 14, 5, 12, 15, 7, 8, 9, 11, 10, 1 };
+            var longestSeq = FindLongestIncreasingSubsequence(sequence);
+            Console.WriteLine("Longest increasing subsequence (LIS)");
+            Console.WriteLine("  Length: {0}", longestSeq.Length);
+            Console.WriteLine("  Sequence: [{0}]", string.Join(", ", longestSeq));
+        }
+
+        public static int[] FindLongestIncreasingSubsequence(int[] sequence)
+        {
+            var len = new int[sequence.Length];
+            var prev = new int[sequence.Length];
+            var maxLen = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                len[i] = 1;
+                prev[i] = -1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (sequence[j] < sequence[i] && len[i] <= len[j])
+                    {
+                        len[i] = len[j] + 1;
+                        prev[i] = j;
+                    }
+                }
+
+                if (len[i] > maxLen)
+                {
+                    maxLen = len[i];
+                    lastIndex = i;
+                }
+
+            }
+
+            var longestSeq = new List<int>();
+            while (lastIndex >= 0)
+            {
+                longestSeq.Add(sequence[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+
+            longestSeq.Reverse();
+            return longestSeq.ToArray();
+        }
+    }
+}
