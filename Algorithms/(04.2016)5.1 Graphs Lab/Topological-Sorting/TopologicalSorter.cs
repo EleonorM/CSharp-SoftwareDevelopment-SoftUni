c640f62ec328482a0092e94b120529dc0c828d43
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-08-12 17:43:15 +0300

    Add solution to Graphs Lab

---
+++ Algorithms/(04.2016)5.1 Graphs Lab/Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+    public ICollection<string> TopSort()
+    {
+        var predecessorCount = new Dictionary<string, int>();
+        foreach (var node in graph)
+        {
+            if (!predecessorCount.ContainsKey(node.Key))
+            {
+                predecessorCount.Add(node.Key, 0);
+            }
+            foreach (var childNode in node.Value)
+            {
+                if (!predecessorCount.ContainsKey(childNode))
+                {
+                    predecessorCount[childNode] = 0;
+                }
+                predecessorCount[childNode]++;
+            }
+        }
+
+        var removedNodes = new List<string>();
+
+        while (true)
+        {
+            var nodeToRemove = graph.Keys.FirstOrDefault(n => predecessorCount[n] == 0);
+            if (nodeToRemove == null)
+            {
+                break;
+            }
+
+            removedNodes.Add(nodeToRemove);
+
+            foreach (var child in this.graph[nodeToRemove])
+            {
+                predecessorCount[child]--;
+            }
+            this.graph.Remove(nodeToRemove);
+        }
+
+        if (graph.Count > 0)
+        {
+            throw new InvalidOperationException("A cycle detected in the graph");
+        }
+
+        return removedNodes;
+    }
+}
